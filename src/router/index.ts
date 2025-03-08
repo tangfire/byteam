189efc8f5d0d6dcb2b3141aca8abe11aafccc471
tangfire <52628216@qq.com>
2025-03-08 19:32:43 +0800

    WebHashHistory

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHashHistory} from 'vue-router'
 
 
 import HomeView from '../components/HomeView.vue'
@@ -11,7 +11,7 @@ import GroupView from "../components/GroupView.vue";
 
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
