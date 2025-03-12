19ee46a6a81361b04b5579d130fdca3702490751
tangfire <52628216@qq.com>
2025-03-12 20:59:55 +0800

    news optimization

--- src/components/NewsView.vue
+++ src/components/NewsView.vue
@@ -28,7 +28,7 @@ const activities2025: ActivityType[] = [
   <div class="container">
     <p style="margin-left: 8px; color: #7d1231; font-size: 26px;">2025</p> <!-- 增加文字大小 -->
 
-  <el-timeline style="max-width: 600px">
+  <el-timeline style="max-width: 800px; margin: 0 auto">
     <el-timeline-item
         v-for="(activity, index) in activities2025"
         :key="index"
@@ -51,13 +51,7 @@ const activities2025: ActivityType[] = [
 
 <style scoped>
 
-.container {
-  max-width: 1200px;
-  margin: 0 auto;
 
-
-
-}
 
 .news-item{
   color: #333; /* 正文文字颜色 */
@@ -69,5 +63,21 @@ const activities2025: ActivityType[] = [
   max-width: 580px;            /* 根据容器宽度优化显示 */
 }
 
+::v-deep(.el-timeline-item) .el-timeline-item__content {
+  white-space: pre-line;
+  word-break: keep-all;
+  overflow-wrap: normal;
+  hyphens: none;
+  max-width: 780px;
+  line-height: 1.5;
+  font-size: 16px;
+  letter-spacing: 0.03em;
+}
+
+.container {
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 0 20px;  /* 增加边距防止贴边 */
+}
 
 </style>
