920bba918be4b174d520528aae02c8441ec60df4
tangfire <52628216@qq.com>
2025-03-12 10:44:04 +0800

    preload currentprojects and publications

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,48 @@ import router from './router'
 import App from './App.vue'
 
 const app = createApp(App)
-const preloadImage = new Image()
-preloadImage.src = '/background/background.png'
+
+
+// 预加载背景图片
+const preloadBackgroundImage = new Image()
+preloadBackgroundImage.src = '/background/background.png'
+
+// 预加载 currentprojects 目录下的图片
+const preloadCurrentProjectsImages = [
+    '/currentprojects/001.ppg',
+    '/currentprojects/002.png',
+    '/currentprojects/003.png',
+    '/currentprojects/004.jpg',
+    '/currentprojects/005.jpg',
+    '/currentprojects/006.png',
+    '/currentprojects/007.png',
+    '/currentprojects/yangbaoyao.jpg',
+];
+
+preloadCurrentProjectsImages.forEach(src => {
+    const img = new Image();
+    img.src = src;
+});
+
+// 预加载 publications 目录下的图片
+const preloadPublicationsImages = [
+    '/publications/001.png',
+    '/publications/002.png',
+    '/publications/003.png',
+    '/publications/004.png',
+    '/publications/005.png',
+    '/publications/006.png',
+    '/publications/007.png',
+    '/publications/008.png',
+    '/publications/009.png',
+];
+
+preloadPublicationsImages.forEach(src => {
+    const img = new Image();
+    img.src = src;
+});
+
+
 app.use(ElementPlus)
 app.use(router)
 app.mount('#app')
